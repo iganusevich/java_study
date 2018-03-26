1a6c5cf4321fc5a633de987039cfa3954d84cc7f
iganusevich <irene.ganusevich@gmail.com>
2018-03-26 14:05:52 +0300

    DistanceTest added

--- Sandbox/src/test/java/pk/test/DistanceTest.java
+++ Sandbox/src/test/java/pk/test/DistanceTest.java
@@ -1,4 +1,18 @@
 package pk.test;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class DistanceTest {
+
+    
+    @Test
+    public void DistanceTest(){
+        pk.test.Point p1 =  new pk.test.Point(0,0);
+        pk.test.Point p2 =  new pk.test.Point(3,4);
+        Assert.assertEquals(p1.distance(p1,p2), 5.0);
+
+
+    }
+
 }
