e5cf019aab5c578868845e5a222f82cd8069cd80
iganusevich <irene.ganusevich@gmail.com>
2018-04-03 18:07:37 +0300

    Click and Type added

--- addressbook-web-tests/src/test/java/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/GroupHelper.java
@@ -4,46 +4,37 @@ import addressbook.models.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupHelper {
-    private FirefoxDriver wd;
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(FirefoxDriver wd) {
-         this.wd = wd;
+        super(wd);
     }
 
     public void returnToGroupPage() {
-        wd.findElement(By.cssSelector("html")).click();
-        wd.findElement(By.cssSelector("html")).click();
-        wd.findElement(By.linkText("group page")).click();
+        click(By.linkText("group page"));
     }
 
     public void submitGroupCreation() {
-        wd.findElement(By.name("submit")).click();
+        click(By.name("submit"));
     }
 
     public void fillInForm(GroupData groupData) {
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+        type(By.name("group_name"), groupData.getName());
+        type(By.name("group_header"), groupData.getHeader());
+        type(By.name("group_footer"), groupData.getFooter());
     }
 
     public void createNewGroup() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[4]")).click();
+        click(By.xpath("//div[@id='content']/form/input[4]"));
     }
 
     public void deleteSelectedGroup() {
-        wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+        click(By.xpath("//div[@id='content']/form/input[5]"));
     }
 
     public void selectGroup() {
         if (!wd.findElement(By.name("selected[]")).isSelected()) {
-            wd.findElement(By.name("selected[]")).click();
+            click(By.name("selected[]"));
         }
     }
 }
