2f31cc9239b3ff12ea14274d9484a349feba5861
iganusevich <irene.ganusevich@gmail.com>
2018-04-20 13:58:59 +0300

    Contact Creation added to Contact Deletion test

--- addressbook-web-tests/src/test/java/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package addressbook.tests;
 
+import addressbook.models.ContactData;
 import org.testng.annotations.Test;
 
 public class ContactDeletionTests extends TestBase{
@@ -7,10 +8,15 @@ public class ContactDeletionTests extends TestBase{
     @Test
     public void testContactDeletion(){
         app.getNavigationHelper().returnToHome();
+        if (!app.getContactHelper().isThereAnyContact()){
+            app.getNavigationHelper().goToAddContactPage();
+            app.getContactHelper().createContact(new ContactData("IreneBD", "Mid", "Test", "Igan", "Title", "Company", "Address", "Home", "Mobile", "Work", "Fax", "E-mail", "E-mail2", "E-mail3", "Homepage","3","February","1991", "5", "March", "2001", "Address 123", "Home 123", "Notes 123"));
+        }
+        app.getNavigationHelper().returnToHome();
         app.getContactHelper().selectDeleteContact();
         app.getContactHelper().submitContactDeletion();
         app.getContactHelper().confirmContactDeletion();
         app.getNavigationHelper().returnToHome();
 
-    }
+    }                                                               
 }
