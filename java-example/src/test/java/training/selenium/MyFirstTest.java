c773eebe96d59abceadac184e51401376ae86820
iganusevich <irene.ganusevich@gmail.com>
2018-11-05 16:16:29 +0200

    #7 Click on menu items

--- java-example/src/test/java/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/training/selenium/MyFirstTest.java
@@ -4,30 +4,23 @@ package training.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.HasCapabilities;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertTrue;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class MyFirstTest  {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
+public class MyFirstTest extends TestBase {
 
 
-    @Before
-    public void start(){
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
-
-    }
 
     @Test
     public void myFirstTest() {
@@ -36,6 +29,22 @@ public class MyFirstTest  {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@value='Login']")).click();
         wait.until(titleIs("My Store"));
+        List<String> menu_names = driver.findElements(By.xpath("//span[@class = 'name']"))
+                .stream()
+                .map((we)-> we.getText())
+                .collect(Collectors.toList());
+        for (String name : menu_names){
+            driver.findElement(By.xpath("//span[.='" + name + "']")).click();
+            assertTrue(areElementsPresent(By.cssSelector("h1")));
+            List<String> sub_menu_names = driver.findElements(By.cssSelector("ul#box-apps-menu ul span"))
+                    .stream()
+                    .map((we)-> we.getText())
+                    .collect(Collectors.toList());
+            for (String sub_name : sub_menu_names){
+                driver.findElement(By.xpath("//span[.='" + sub_name + "']")).click();
+                assertTrue(areElementsPresent(By.cssSelector("h1")));
+            }
+        }
     }
 
     @After
--- java-example/src/test/java/training/selenium/TestBase.java
+++ java-example/src/test/java/training/selenium/TestBase.java
@@ -2,17 +2,25 @@ package training.selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
     public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
     public WebDriver driver;
     public WebDriverWait wait;
+
+    public boolean areElementsPresent(By locator ){
+        return driver.findElements(locator).size()>0;
+    }
 
     @Before
     public void start() {
@@ -21,20 +29,18 @@ public class TestBase {
             wait = new WebDriverWait(driver, 10);
             return;
         }
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(FirefoxDriver.MARIONETTE, false);
-        driver = new FirefoxDriver(caps);
+        //DesiredCapabilities caps = new DesiredCapabilities();
+        //caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         tlDriver.set(driver);
         System.out.println(((HasCapabilities) driver).getCapabilities());
         wait = new WebDriverWait(driver, 10);
-
-        Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> { driver.quit(); driver = null; }));
     }
 
     @After
     public void stop() {
-        //driver.quit();
-        //driver = null;
+        driver.quit();
+        driver = null;
     }
 }
