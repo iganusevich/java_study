2668ef3bec2ba13775744ddafda814acdddd8b2e
iganusevich <irene.ganusevich@gmail.com>
2018-12-12 17:40:02 +0200

    Test Corrected

--- sifma-tests/src/test/java/tests/SmokeTests.java
+++ sifma-tests/src/test/java/tests/SmokeTests.java
@@ -95,7 +95,7 @@ public class SmokeTests extends TestBase {
     @Test
     public void advisorLogsInAsPlayer(){
         SoftAssert asert=new SoftAssert();
-        Advisor advisor = new Advisor().withLogin("SIA_99_T798519/").withPassword("BQXA4927"); // Prod: SIA_99_T798519 / BQXA4927
+        Advisor advisor = new Advisor().withLogin("SIA_99_T798519").withPassword("BQXA4927"); // Prod: SIA_99_T798519 / BQXA4927
         app.getAdvisorHelper().loginAsAdvisor(advisor);
         app.getAdvisorHelper().closeNewsPopUp();
         String mainWindow = app.getAdvisorHelper().getCurrentWindow();
