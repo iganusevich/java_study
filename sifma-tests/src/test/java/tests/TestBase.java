ecc7227088b5081a462377fca77c3312bd525834
iganusevich <irene.ganusevich@gmail.com>
2018-12-17 14:09:25 +0200

    Added Logger

--- sifma-tests/src/test/java/tests/TestBase.java
+++ sifma-tests/src/test/java/tests/TestBase.java
@@ -4,9 +4,12 @@ package tests;
 import appmanager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -16,6 +19,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 @Listeners(MyTestListener.class)
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app = new ApplicationManager(
             System.getProperty("browser", BrowserType.CHROME));
@@ -29,6 +34,16 @@ public class TestBase {
     @AfterSuite  (alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStert(Method m, Object[] p){
+        logger.info("Start test" + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method m){
+        logger.info("Stop test" + m.getName());
     }
 
 }
